c90f969bb75c3a7b41a8151758ced7dee413d3d0
Darri Steinn Konradsson <darri15@hotmail.com>
2016-09-02 17:20:04 +0000

    adding solution setup

---
+++ solution-setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "Please choose a solution name (Camel casing)!"
+    exit
+fi
+
+mkdir $1-solution
+cd $1-solution
+mkdir -p src/API src/Models src/Services/Itf src/Services/Imp src/Entities/Models/DTOModels src/Entities/Models/ViewModels src/Entities/Data test/Test-Library
+touch global.json
+cat << EOT >> global.json
+{
+    "projects": [
+        "src",
+        "test"
+    ]
+}
+EOT
+cd test/Test-Library
+dotnet new -t xunittest
+dotnet restore
+dotnet build
+
+cd ../../src/API
+dotnet new -t Web
+astyle Program.cs
+astyle Startup.cs
+dotnet restore
+dotnet build
+
+cd ../Models
+dotnet new -t lib
+rm Library.cs
+dotnet restore
+dotnet build
+cp ../../../Templates/projectModels.json project.json
+
+cd ../Services
+dotnet new -t lib
+rm Library.cs
+dotnet restore
+dotnet build
+cp ../../../Templates/projectServices.json project.json
+
+cd ../Entities
+dotnet new -t lib
+rm Library.cs
+dotnet restore
+dotnet build
+cp ../../../Scripts/setup-database.sh .
+cp ../../../Templates/projectEntities.json project.json
